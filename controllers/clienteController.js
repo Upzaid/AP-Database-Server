0b21ed5af5c1f70239e79d08dcc102b22df2a213
Zaid <zaid_lorea@yahoo.com.mx>
2021-05-03 15:26:51 -0500

    Added cliente and naviera coontroller for get, edit and post routes

---
+++ controllers/clienteController.js
@@ -0,0 +1,99 @@
+const tokenAuthentication = require('../tokenAuthentication')
+const validation = require('../validation').navieraValidation
+const Cliente = require('../models/cliente')
+
+// List of clientes
+
+exports.cliente_list = async (req, res) =>{
+    
+    const response = await Cliente.find().sort({razon_social: 1}) 
+
+    res.json(response.map(cliente =>{
+        const {clave, razon_social, rfc, domicilio, telefono} = cliente
+        return (
+            {
+                clave,
+                razon_social,
+                rfc,
+                domicilio,
+                telefono
+            }
+        )
+    }))
+}
+
+// Create a cliente
+
+exports.cliente_create = async (req, res) =>{
+    
+    // Validate inputs
+    const {error} = validation.validate(req.body)
+    if (error) return res.status(403).json(error.details[0].message)
+
+    // Verify clave is not already used
+    
+    const response = await Cliente.findOne({clave: req.body.clave})
+    if (!response){
+        const {clave, razon_social, rfc, domicilio, telefono} = req.body
+        
+        const cliente = new Cliente({
+            clave,
+            razon_social,
+            rfc,
+            domicilio,
+            telefono
+        })
+        cliente.save()
+        return res.json('Cliente creado exitosamente')
+    }
+    res.status(403).json(`La clave de cliente ${req.body.clave} esta asignada`)
+}
+
+// Delete cliente
+exports.cliente_delete = async (req, res) =>{
+    
+    const response = await cliente.findOne({clave: req.body.clave}, {useFindAndModify: false})
+    
+    if (response) {
+        await Cliente.findByIdAndRemove(response.id)
+        return res.json('Cliente borrada exitosamente')
+    }
+
+    res.status(403).json('Cliente no existe')
+}
+
+// Edit cliente
+exports.cliente_edit = async (req, res) =>{
+    const response = await cliente.findOne({clave: req.body.clave})
+    
+    if (response) {
+        const {clave, razon_social, rfc, domicilio, telefono} = req.body
+       
+        const cliente = new Cliente({
+            clave,
+            razon_social,
+            rfc,
+            domicilio,
+            telefono,
+            _id: response.id
+        })
+        await Cliente.findByIdAndUpdate(response.id, cliente, {useFindAndModify: false})
+        return res.json('Cliente actulizada exitosamente')
+    }
+
+    res.status(403).json('Cliente no existe')
+}
+
+// Find cliente by clave
+exports.cliente_find = async (req, res) =>{
+    try {
+        const response = await Cliente.findOne({clave: req.params.clave})
+        if (response){
+            return res.json(response)
+        }
+    } catch (error) {
+        return res.send(error)
+    }
+
+    res.send('Cliente no encontrado')
+}
---
+++ controllers/navieraController.js
@@ -0,0 +1,100 @@
+const tokenAuthentication = require('../tokenAuthentication')
+const validation = require('../validation').navieraValidation
+const Naviera = require('../models/naviera')
+
+// List of navieras
+
+exports.naviera_list = async (req, res) =>{
+    
+    const response = await Naviera.find().sort({razon_social: 1}) 
+
+    res.json(response.map(naviera =>{
+        const {clave, razon_social, rfc, domicilio, telefono} = naviera
+        return (
+            {
+                clave,
+                razon_social,
+                rfc,
+                domicilio,
+                telefono
+            }
+        )
+    }))
+}
+
+// Create a naviera
+
+exports.naviera_create = async (req, res) =>{
+    
+    // Validate inputs
+    const {error} = validation.validate(req.body)
+    if (error) return res.status(403).json(error.details[0].message)
+
+    // Verify clave is not already used
+    
+    const response = await Naviera.findOne({clave: req.body.clave})
+    if (!response){
+        const {clave, razon_social, rfc, domicilio, telefono} = req.body
+        
+        const naviera = new Naviera({
+            clave,
+            razon_social,
+            rfc,
+            domicilio,
+            telefono
+        })
+        naviera.save()
+        return res.json('Naviera creada exitosamente')
+    }
+    res.status(403).json(`La clave de naviera ${req.body.clave} esta asignada`)
+}
+
+// Delete naviera
+exports.naviera_delete = async (req, res) =>{
+    
+    const response = await Naviera.findOne({clave: req.body.clave}, {useFindAndModify: false})
+    
+    if (response) {
+        await Naviera.findByIdAndRemove(response.id)
+        return res.json('Naviera borrada exitosamente')
+    }
+
+    res.status(403).json('Naviera no existe')
+}
+
+// Edit naviera
+exports.naviera_edit = async (req, res) =>{
+    const response = await Naviera.findOne({clave: req.body.clave})
+    
+    if (response) {
+        const {clave, razon_social, rfc, domicilio, telefono} = req.body
+       
+        const naviera = new Naviera({
+            clave,
+            razon_social,
+            rfc,
+            domicilio,
+            telefono,
+            _id: response.id
+        })
+        await Naviera.findByIdAndUpdate(response.id, naviera, {useFindAndModify: false})
+        return res.json('Naviera actulizada exitosamente')
+    }
+
+    res.status(403).json('Naviera no existe')
+}
+
+// Find naviera by clave
+
+exports.naviera_find = async (req, res) =>{
+    try {
+        const response = await Naviera.findOne({clave: req.params.clave})
+        if (response){
+            return res.json(response)
+        }
+    } catch (error) {
+        return res.send(error)
+    }
+
+    res.send('Naviera no encontrado')
+}
