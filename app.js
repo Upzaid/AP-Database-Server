5aa508557b8c497aea7c48075b1aab1c73572ac0
Zaid <zaid_lorea@yahoo.com.mx>
2021-05-11 17:05:52 -0500

    Added routes for mantenimiento

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 // Route imports
+const userRouter = require('./routes/user')
+const navieraRouter = require('./routes/naviera')
+const clienteRouter = require('./routes/cliente')
+const personalRouter = require('./routes/personal')
+const anticipoRouter = require('./routes/anticipo')
+const unidadRouter = require('./routes/unidad')
+const ordenRouter = require('./routes/orden')
+const liquidacionRouter = require('./routes/liquidacion')
+const mantenimientoRouter = require('./routes/mantenimiento')
 
 // Initilize express
 
@@ -15,15 +24,23 @@ const mongoDB = process.env.DB
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => console.log('Database conenction successful'));
+db.once('open', () => console.log('Database connection successful'));
 
 // Middleware
 app.use(express.json())
 app.use(cors())
 
 // Routes
-
 app.get('/', (req, res) => res.json('Conexion exitosa'))
+app.use('/user', userRouter)
+app.use('/naviera', navieraRouter)
+app.use('/cliente', clienteRouter)
+app.use('/personal', personalRouter)
+app.use('/anticipo', anticipoRouter)
+app.use('/unidad', unidadRouter)
+app.use('/orden', ordenRouter)
+app.use('/liquidacion', liquidacionRouter)
+app.use('/mantenimiento', mantenimientoRouter)
 
 
 app.listen(process.env.PORT || 5000, ()=> console.log(`Server running on port ${process.env.PORT || 5000}`))
---
+++ routes/mantenimiento.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+
+const tokenAuthentication = require('../tokenAuthentication')
+const validation = require('../validation')
+
+const mantenimiento_controller = require('../controllers/mantenimientoController')
+
+// Get all mantenimiento documents
+router.get('/list', tokenAuthentication, mantenimiento_controller.mantenimiento_list)
+
+// Create mantenimiento order
+router.post('/create', tokenAuthentication, validation.editValidation, mantenimiento_controller.mantenimiento_create)
+
+// Delete mantenimiento order
+router.delete('/delete', tokenAuthentication, validation.editValidation, mantenimiento_controller.mantenimiento_delete)
+
+// Delete mantenimiento order
+router.put('/edit', tokenAuthentication, validation.editValidation, mantenimiento_controller.mantenimiento_edit)
+
+// Latest mantenimiento order
+router.get('/latest', tokenAuthentication, mantenimiento_controller.mantenimiento_latest)
+
+// Find mantenimiento order by folio
+router.get('/find/:folio', tokenAuthentication, mantenimiento_controller.mantenimiento_find)
+
+module.exports = router
